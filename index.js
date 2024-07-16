9c3926d9371c5b0c8a879648b67628982cae93ac
jawid fakhari <jawid_fakhari@live.it>
2024-07-16 12:03:02 +0200

    HemisphereLight, wireframe mesh as child to main mesh, OrbitControls class imported, orbitcontrols object, Damping option added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import necessary modules and classes from Three.js
-import * as THREE from 'three';
+import * as THREE from "three";
+// Import orbite controller
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 // width and height of the canvas
 const w = window.innerWidth;
@@ -21,18 +23,24 @@ const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 // set the camera position
-camera.position.z = 5;
+camera.position.z = 2;
 // camera.position.x = 2;
 // camera.position.y = 3;
 
 // create a scene
 const scene = new THREE.Scene();
 
+// create an OrbitControls object
+const controls = new OrbitControls(camera, renderer.domElement);
+// enable damping for better performance
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 // create a sphera geometry
 const geometry = new THREE.IcosahedronGeometry(1.0, 2);
 // create a material with a azur color
-const mat = new THREE.MeshBasicMaterial({
-    color: 0x4488ff
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
 });
 
 // create a mesh with the geometry and material
@@ -40,17 +48,27 @@ const mesh = new THREE.Mesh(geometry, mat);
 // add the mesh to the scene
 scene.add(mesh);
 
-
+// create a wireframe version of the mesh
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+})
+// create a wireframe mesh with the same geometry and material
+const wireMesh = new THREE.Mesh(geometry, wireMat);
+// add the wireframe mesh as a child of the main mesh
+mesh.add(wireMesh);
+// light the scene
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500, 1);
+scene.add(hemiLight);
 // animate the scene
-
 function animate() {
-    // api call the animate function again after the next frame of the animation
-    requestAnimationFrame(animate);
-
-    // rotate the mesh
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    // render the scene to the canvas from the perspective of the camera
-    renderer.render(scene, camera);
+  // api call the animate function again after the next frame of the animation
+  requestAnimationFrame(animate);
+  // rotate the mesh
+  mesh.rotation.y += 0.001;
+  // render the scene to the canvas from the perspective of the camera
+  renderer.render(scene, camera);
+  // required after each render for OrbitControls
+  controls.update(); 
 }
 animate();
